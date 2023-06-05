15adb7862808641447a451fa13a72941e028dea0
koval <kovalevsaska@bk.ru>
2023-06-05 19:04:25 +0300

    DDD

---
+++ static/js/slider.js
@@ -0,0 +1,12 @@
+const swiper= new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 6,
+  slidesPerGroup: 1,
+  spaceBetween: 10,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
