6341964167456c040897fa63eb630f106e9341dc
MotoIlyuha <motoilyuha@mail.ru>
2023-06-05 19:32:07 +0300

    Перенес карточки в отдельный файл, организовал jinja micro. Адаптив для слайдера

--- app.py
+++ app.py
@@ -2,9 +2,41 @@ from flask import render_template, request, redirect, jsonify
 from create_db import *
 
 
+def get_additives(synonym, limit=50):
+    matching_synonyms = Synonym.query.filter(Synonym.name.like(f'%{synonym}%')).all()
+
+    # Собрать идентификаторы связанных синонимами объектов Data
+    data_ids = [synonym.data_id for synonym in matching_synonyms]
+
+    # Получить данные Data и связанные с ними объекты Danger, Category и Origin
+    data_query = Data.query \
+        .join(Danger) \
+        .join(Category, Data.categories) \
+        .join(Origin, Data.origins) \
+        .filter(Data.id.in_(data_ids)) \
+        .order_by(Data.code) \
+        .limit(limit)
+
+    # Извлечь необходимые поля из результата запроса
+    result = data_query.with_entities(Data.code, Data.name, Danger.name, Category.name, Origin.name).all()
+
+    # Преобразовать результат в список словарей для удобства использования
+    data = []
+    for row in result:
+        data.append({
+            'code': row[0],
+            'name': row[1],
+            'danger': row[2],
+            'categories': row[3:],
+            'origins': row[3:]
+        })
+    return data
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = get_additives("")
+    return render_template('index.html', additives=data)
 
 
 @app.route('/additives')
@@ -23,32 +55,7 @@ def additives_category(category):
 @app.route('/additives/process', methods=['POST'])
 def process():
     text = request.form['text']
-    matching_synonyms = Synonym.query.filter(Synonym.name.like(f'%{text}%')).all()
-
-    # Собрать идентификаторы связанных синонимами объектов Data
-    data_ids = [synonym.data_id for synonym in matching_synonyms]
-
-    # Получить данные Data и связанные с ними объекты Danger, Category и Origin
-    data_query = Data.query \
-        .join(Danger) \
-        .join(Category, Data.categories) \
-        .join(Origin, Data.origins) \
-        .filter(Data.id.in_(data_ids))\
-        .order_by(Data.code)
-
-    # Извлечь необходимые поля из результата запроса
-    result = data_query.with_entities(Data.code, Data.name, Danger.name, Category.name, Origin.name).all()
-
-    # Преобразовать результат в список словарей для удобства использования
-    data = []
-    for row in result:
-        data.append({
-            'code': row[0],
-            'name': row[1],
-            'danger': row[2],
-            'categories': row[3:],
-            'origins': row[3:]
-        })
+    data = get_additives(text)
     return jsonify(data)
 
 
